56de29ff3ac522dcbe86064bd4eac5b3310a5561
alifzwan <alifzakwan529@gmail.com>
2024-06-15 21:43:56 +0800

    chore: Update Express server configuration and add API endpoint for date conversion

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+require('dotenv').config();
 const express = require('express');
 const app = express();
 
@@ -6,23 +6,50 @@ const app = express();
 const cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
+ 
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + '/views/index.html');
 });
 
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+const isInvalidDate = (date) => {
+  date.toUTCString() === "Invalid Date";
+}
+
+
+// Your first API endpoint
+app.get("/api/:date", (request, response) => {
+  let date = new Date(request.params.date);
+  
+  if (isInvalidDate(date)) { // if date is invalid
+    date = new Date(+request.params.date); // convert to number
+  }
+
+  // if date is still invalid, return error
+  if(isInvalidDate(date)) {
+    response.json({ error: "Invalid Date" });
+    return
+  }
+
+  response.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  }) 
 });
 
+app.get('/', (request, response) => {
+  response.json({
+    unix: new Date().getTime(),
+    utc: new Date().toUTCString()
+  });
+})
 
 
-const PORT = process.env.PORT || 3000
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+
+
+const listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
 });
