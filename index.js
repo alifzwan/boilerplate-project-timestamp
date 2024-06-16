a9553c6826ed0ce6f1fa0901d1316f9545bfe5d6
alifzwan <alifzakwan529@gmail.com>
2024-06-16 18:20:08 +0800

    chore: Refactor date parsing logic in API endpoint

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-require('dotenv').config();
 const express = require('express');
 const app = express();
+
+
 
 
 const cors = require('cors');
@@ -14,38 +15,36 @@ app.get("/", (request, response) => {
   response.sendFile(__dirname + '/views/index.html');
 });
 
-
 const isInvalidDate = (date) => {
-  date.toUTCString() === "Invalid Date";
+  return isNaN(date.getTime());
 }
 
 
-// Your first API endpoint
 app.get("/api/:date", (request, response) => {
-  let date = new Date(request.params.date);
-  
-  if (isInvalidDate(date)) { // if date is invalid
-    date = new Date(+request.params.date); // convert to number
+
+  let date 
+
+  if(request.params.date) {
+    if(!isNaN(request.params.date)) {
+      date = new Date(parseInt(request.params.date))
+    } else {
+      date = new Date(request.params.date)
+    }
+  } else {
+    date = new Date()
   }
 
-  // if date is still invalid, return error
   if(isInvalidDate(date)) {
-    response.json({ error: "Invalid Date" });
-    return
+    response.json({ error: "Invalid Date" })
+  } else {
+    response.json({ 
+      unix: date.getTime(), 
+      utc: date.toUTCString() 
+    })
   }
-
-  response.json({
-    unix: date.getTime(),
-    utc: date.toUTCString()
-  }) 
-});
-
-app.get('/', (request, response) => {
-  response.json({
-    unix: new Date().getTime(),
-    utc: new Date().toUTCString()
-  });
 })
+
+
 
 
 
